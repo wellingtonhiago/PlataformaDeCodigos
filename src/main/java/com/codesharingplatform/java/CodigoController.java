8052a337297501f0edde8061b8c53900ff2ffe5f
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-10-19 23:49:47 -0300

    Desenvolvimento do estágio 2 de 5

--- src/main/java/com/codesharingplatform/java/CodigoController.java
+++ src/main/java/com/codesharingplatform/java/CodigoController.java
@@ -1,28 +1,46 @@
 package com.codesharingplatform.java;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+
 
 @RestController
 public class CodigoController {
+
+    private static final String DATA_FORMATADA= "yyyy/MM/dd HH:mm:ss";
 
     private final String codigo = "public static void main(String[] args) {\n    SpringApplication.run(CodeSharingPlatform.class, args);\n}";
 
     private final List<ApiCodigos> listaApi = List.of(new ApiCodigos(codigo));
 
     @GetMapping("/api/code")
-    public ApiCodigos getApiCodigos() {
+    public ApiCodigos apiCodigos() {
         return listaApi.get(0);
     }
 
-
-    private final List<Codigos> listaCodigo = List.of(new Codigos(codigo));
-
     @GetMapping("/code")
-    public String getCodigos() {
-        return listaCodigo.get(0).getWebCodigo();
+    public ModelAndView codigo() {
+        ModelAndView mv = new ModelAndView();
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern(DATA_FORMATADA);
+        String data = now.format(formato);
+
+        mv.setViewName("codigo");
+        mv.addObject("codigoData", data);
+        mv.addObject("codigo", listaApi.get(0).getCode());
+
+        return mv;
+    }
+
+    @PostMapping("/api/code/new")
+    public String postCodigo(@RequestBody String codigo) {
+        listaApi.get(0).setCode(codigo);
+        return "{}";
     }
 
 }
--- src/main/java/com/codesharingplatform/java/PlataformaDeCodigos.java
+++ src/main/java/com/codesharingplatform/java/PlataformaDeCodigos.java
@@ -3,8 +3,6 @@ package com.codesharingplatform.java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-
 @SpringBootApplication  // obter todos os recursos fornecidos pelo Spring Boot
 public class PlataformaDeCodigos {
 
