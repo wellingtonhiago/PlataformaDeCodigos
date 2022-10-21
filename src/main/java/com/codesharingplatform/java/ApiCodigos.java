49f84f2c9e3631b6e1f4ab1dd15d1978bddd208e
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-10-21 00:36:46 -0300

    Desenvolvimento do estágio 2 de 5

--- src/main/java/com/codesharingplatform/java/ApiCodigos.java
+++ src/main/java/com/codesharingplatform/java/ApiCodigos.java
@@ -1,8 +1,12 @@
 package com.codesharingplatform.java;
 
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ApiCodigos {
     private String code;
+    private String date;
 
     public ApiCodigos() {}
 
@@ -18,4 +22,15 @@ public class ApiCodigos {
         this.code = code;
     }
 
+    public String getDate() {
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String date = now.format(formato);
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
--- src/main/java/com/codesharingplatform/java/CodigoController.java
+++ src/main/java/com/codesharingplatform/java/CodigoController.java
@@ -11,8 +11,6 @@ import java.util.List;
 @RestController
 public class CodigoController {
 
-    private static final String DATA_FORMATADA= "yyyy/MM/dd HH:mm:ss";
-
     private final String codigo = "public static void main(String[] args) {\n    SpringApplication.run(CodeSharingPlatform.class, args);\n}";
 
     private final List<ApiCodigos> listaApi = List.of(new ApiCodigos(codigo));
@@ -26,12 +24,8 @@ public class CodigoController {
     public ModelAndView codigo() {
         ModelAndView mv = new ModelAndView();
 
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern(DATA_FORMATADA);
-        String data = now.format(formato);
-
         mv.setViewName("codigo");
-        mv.addObject("codigoData", data);
+        mv.addObject("codigoData", listaApi.get(0).getDate());
         mv.addObject("codigo", listaApi.get(0).getCode());
 
         return mv;
